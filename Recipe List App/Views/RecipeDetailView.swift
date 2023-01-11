8964ef25fd4ce7a1318af31d9de0ff9d3a20cb40
walread <will.alread@gmail.com>
2023-01-11 12:17:10 -0500

    Button for recipe card in FeaturedView. Ingredient calculation and presentation.

--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe: Recipe
+    @State var selectedServingSize = 2
     
     var body: some View {
         ScrollView {
@@ -19,6 +20,26 @@ struct RecipeDetailView: View {
                     .resizable()
                     .scaledToFill()
                 
+                // MARK: Recipe title
+                Text(recipe.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                
+                // MARK: Serving Size Picker
+                VStack(alignment: .leading) {
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(.segmented)
+                    .frame(width: 160)
+                }
+                .padding()
+                
                 // MARK: Ingredients
                 VStack(alignment: .leading) {
                     Text("Ingredients")
@@ -26,7 +47,7 @@ struct RecipeDetailView: View {
                         .padding([.bottom, .top], 5)
                     
                     ForEach (recipe.ingredients) { item in
-                        Text("• " + item.name)
+                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
                     }
                 }
                 .padding(.horizontal)
@@ -47,7 +68,7 @@ struct RecipeDetailView: View {
                 }
                 .padding(.horizontal)
             }
-        }.navigationBarTitle(recipe.name)
+        }
     }
 }
 
--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model: RecipeModel
+    @State var isDetailViewShowing = false
     
     var body: some View {
         
@@ -31,19 +32,34 @@ struct RecipeFeaturedView: View {
                         // Only show those that should be featured
                         if model.recipes[index].featured {
                             
-                            // Recipe card
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                    
-                                VStack(spacing: 0) {
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
+                            // Recipe card button
+                            Button(action: {
+                                
+                                // Show the recipe detail sheet
+                                self.isDetailViewShowing = true
+                                
+                            }, label: {
+                                
+                                // Recipe card
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                        
+                                    VStack(spacing: 0) {
+                                        Image(model.recipes[index].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .clipped()
+                                        Text(model.recipes[index].name)
+                                    }
                                 }
+                            })
+                            .sheet(isPresented: $isDetailViewShowing) {
+                                
+                                // Show the Recipe Detail View
+                                RecipeDetailView(recipe: model.recipes[index])
                             }
+                            .buttonStyle(PlainButtonStyle())
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
                             .cornerRadius(15)
                             .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
